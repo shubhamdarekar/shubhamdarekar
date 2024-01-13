a606fe372a94b6a5c050630c148ea26788ff614b
Shubham Darekar <shubham.darekar5@gmail.com>
2024-01-12 19:21:41 -0800

    Updates

--- JS/contactButton.js
+++ JS/contactButton.js
@@ -46,14 +46,14 @@ function hide() {
     contactIcon.classList.add("showIcon");
 }
 firstPlatform.addEventListener('click',()=>{
-    window.open("https://twitter.com/manvar_prit");
+    window.open("https://twitter.com/shubhamdarekar");
 });
 secondPlatform.addEventListener('click',()=>{
-    window.open("mailto:pritmanvar1109@gmail.com");
+    window.open("mailto:shubham.darekar5@gmail.com");
 });
 thirdPlatform.addEventListener('click',()=>{
-    window.open("https://github.com/pritmanvar");
+    window.open("https://github.com/shubhamdarekar");
 });
 fourthPlatform.addEventListener('click',()=>{
-    window.open("https://www.linkedin.com/in/prit-manvar-36ab97204/");
+    window.open("https://www.linkedin.com/in/shubhamdarekar/");
 });
